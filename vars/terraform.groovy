cface4a674ddb79c8c51fab01ee4ddc962eeaf22
jason-gumbs <wilfred.smithandsmith@gmail.com>
2021-11-12 01:17:50 -0500

    initial

---
+++ vars/terraform.groovy
@@ -0,0 +1,20 @@
+podTemplate(label: 'terraform',
+        containers: [
+                containerTemplate(name: 'terraform', image: 'hashicorp/terraform:1.0.11', ttyEnabled: true, command: 'cat')
+        ]) {
+        checkout scm
+        node("terraform") {
+            checkout scm
+            container("terraform") {
+                stage('terraform init') {
+                    sh "terraform init"
+                }
+                stage('terraform plan') {
+                    sh "terraform plan"
+                }
+                stage('terraform init') {
+                    sh "terraform init"
+                }
+            }
+        }
+    }
