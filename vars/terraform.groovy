7867bc7a7a64128dfd8345459187fef0299b0360
jason-gumbs <wilfred.smithandsmith@gmail.com>
2021-11-12 01:54:07 -0500

    update

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,20 +1,22 @@
-node {
-    podTemplate(label: 'terraform',
-            containers: [
-                    containerTemplate(name: 'terraform', image: 'hashicorp/terraform:1.0.11', ttyEnabled: true, command: 'cat')
-            ]) {
-        checkout scm
-        node("terraform") {
+def call() {
+    node {
+        podTemplate(label: 'terraform',
+                containers: [
+                        containerTemplate(name: 'terraform', image: 'hashicorp/terraform:1.0.11', ttyEnabled: true, command: 'cat')
+                ]) {
             checkout scm
-            container("terraform") {
-                stage('terraform init') {
-                    sh "terraform init"
-                }
-                stage('terraform plan') {
-                    sh "terraform plan"
-                }
-                stage('terraform init') {
-                    sh "terraform init"
+            node("terraform") {
+                checkout scm
+                container("terraform") {
+                    stage('terraform init') {
+                        sh "terraform init"
+                    }
+                    stage('terraform plan') {
+                        sh "terraform plan"
+                    }
+                    stage('terraform init') {
+                        sh "terraform init"
+                    }
                 }
             }
         }
